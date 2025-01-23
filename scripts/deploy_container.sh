e4de91e3d638c1123925ec2022db6a631eb70de3
FeiWang <fei.wang@spatialfront.com>
2025-01-23 15:51:10 -0500

    add .sh file

---
+++ scripts/deploy_container.sh
@@ -0,0 +1,3 @@
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin "$AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com"
+docker pull "$AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/gsa-comet-prod-spring-backend:a571ee2"
+docker run -d --name spring-app -p 8080:8080 "$AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/gsa-comet-prod-spring-backend:a571ee2"
